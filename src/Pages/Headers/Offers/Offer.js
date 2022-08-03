146088ab6d8c18519b8825f97467bf90ba3541b7
Vasanth1650 <vasanth16756@gmail.com>
2022-08-03 11:43:32 +0530

    dwwad

--- src/Pages/Headers/Offers/Offer.js
+++ src/Pages/Headers/Offers/Offer.js
@@ -17,15 +17,15 @@ function Offer() {
   return (
     <div>
         <BootStrap.Carousel className='offer' >
-                <BootStrap.Carousel.Item interval={4000} onClick={()=>dummy()}>
+                <BootStrap.Carousel.Item interval={5000} onClick={()=>dummy()}>
                     <div className='offertext'>Save Up to 40%</div>
                     <div className='offertext1'>Shop All Our New Markdowns</div>
                 </BootStrap.Carousel.Item>
-                <BootStrap.Carousel.Item interval={4000} onClick={()=>dummy()}>
+                <BootStrap.Carousel.Item interval={5000} onClick={()=>dummy()}>
                 <div className='offertext'>Students Now Get 10% Off</div>
                 <div className='offertext1'>Learn more</div>
                 </BootStrap.Carousel.Item>
-                <BootStrap.Carousel.Item interval={4000} onClick={()=>dummy1()}>
+                <BootStrap.Carousel.Item interval={5000} onClick={()=>dummy1()}>
                 <div className='offertext'>It's taking us a bit longer than usual to get your order to you. Thank you for your patience.</div>
                 <div className='offertext1'>Learn more</div>
                 </BootStrap.Carousel.Item>

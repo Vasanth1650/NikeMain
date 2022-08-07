d66ec3bd284ad2399dce874497266031fdf0b6ed
Vasanth1650 <vasanth16756@gmail.com>
2022-08-07 13:26:36 +0530

    dwwadw

--- src/Pages/LiveChat/Livechat.js
+++ src/Pages/LiveChat/Livechat.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useState } from 'react';
 import io from 'socket.io-client';
 import Footer from '../Footer/Footer';
@@ -6,8 +6,6 @@ import Header from '../Headers/Header';
 import Chat from './Chat';
 import './Styles/Chat.css';
 import ReactPlayer from 'react-player';
-import { fetchUserData } from '../../Api/AuthenticationService';
-import { useNavigate } from 'react-router-dom';
 
 const socket = io.connect("https://nikelivechat.herokuapp.com");
 
@@ -16,34 +14,6 @@ function Livechat() {
   const [username, setUsername] = useState("");
   const [room, setRoom] = useState("");
   const [showChat, setShowChat] = useState(false);
-  const usenavigate = useNavigate()
-  const [data, setData] = useState({})
-
-  useEffect(() => {
-    if (!localStorage.getItem("USER_KEY")) {
-      usenavigate('/login')
-    }
-  }, [])
-
-  
-
-  useEffect(() => {
-    if (data.roleCode === "ADMIN") {
-      setUsername(data.email)
-    } else if (data.roleCode === "USER") {
-      setUsername(data.email)
-      setRoom(data.id)
-    }
-  }, [data])
-
-  React.useEffect(() => {
-    fetchUserData().then((response) => {
-      setData(response.data)
-      console.log(response.data)
-    }).catch((err) => {
-      console.log(err)
-    })
-  }, [])
 
 
 
@@ -65,13 +35,13 @@ function Livechat() {
   };
 
 
-console.log(room)
+
 
   return (
     <div className="Apphjbj">
       <Header />
       <div>
-        <br />
+        <br/>
         <div className='featuesa'>ONE VIDEO FOR SURE YOU GONNAA LOVE IT</div>
 
         <div className='knjdwanjdna' style={{ position: "static" }}>
@@ -83,7 +53,7 @@ console.log(room)
         <div className='smalltext'>IS GETTING THE RECOGNITION TO MY WORK</div>
       </div>
 
-      <br /><br />
+      <br/><br/>
       <div className='featuesa'>CUSTOMER SUPPORT CHANNEL THATS IT THE SITE ENDS HERE</div>
 
       {!showChat ? (
@@ -92,23 +62,10 @@ console.log(room)
           <input
             type="text"
             placeholder="Enter Your Name"
-            value={data.username}
             onChange={(event) => {
               setUsername(event.target.value);
             }}
           />
-          {data.roleCode==="USER" &&
-          <input
-            type="text"
-            placeholder="Room ID..."
-            value={data.id}
-            onChange={(event) => {
-              setRoom(event.target.value);
-            }}
-
-          />}
-
-          {data.roleCode==="ADMIN" &&
           <input
             type="text"
             placeholder="Room ID..."
@@ -116,14 +73,12 @@ console.log(room)
               setRoom(event.target.value);
             }}
 
-          />}
-
+          />
           <button onClick={joinRoom}>Join A Room</button>
         </div>
       ) : (
         <Chat socket={socket} username={username} room={room} />
       )}
-      <br /><br /><br /><br /><br /><br />
       <Footer />
     </div>
   )
